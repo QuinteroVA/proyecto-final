a3c9c978a6d1e0e2b0f93649e608ca33a4e90c63
Leonardo Santo <leonard_sc@hotmail.com>
2023-07-27 19:54:21 -0500

    Actualizacion ejemplo

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
