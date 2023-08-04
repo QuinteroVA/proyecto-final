3355ab2d586da65bdf74eb661b60a88db9f439ed
CentauroPro <centauro_pro1989@gmail.com>
2023-08-03 21:31:57 -0500

    pantalla Inicio

--- navigators/MainNavigator.js
+++ navigators/MainNavigator.js
@@ -8,60 +8,88 @@ import RegScreen from "../screens/RegScreen";
 import CuentaScreen from "../screens/CuentaScreen";
 import ProductsScreen from "../screens/ProductsScreen";
 import CarritoScreen from "../screens/CarritoScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 //iconos
 
-
-
-
-import { Feather } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 function MyTabs() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Inicio" component={WelcomeScreen} options={{
-                  
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Inicio"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: () => (
+            <Feather name="home" size={30} color={"#040b04fd"} />
+          ),
+        }}
+      />
 
-                    tabBarIcon: () => (
-                        <Feather name="home" size={30} color={'#040b04fd'} />)}}/>
+      <Tab.Screen
+        name="Productos"
+        component={ProductsScreen}
+        options={{
+          tabBarIcon: () => (
+            <MaterialCommunityIcons
+              name="propane-tank"
+              size={24}
+              color="black"
+            />
+          ),
+        }}
+      />
 
-            
-            
-            <Tab.Screen name="Productos" component={ProductsScreen} options={{
-                 
+      <Tab.Screen
+        name="Carrito"
+        component={CarritoScreen}
+        options={{
+          tabBarIcon: () => (
+            <MaterialCommunityIcons
+              name="shopping-search"
+              size={24}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Mi Cuenta"
+        component={CuentaScreen}
+        options={{
+          tabBarIcon: () => (
+            <MaterialCommunityIcons
+              name="account-circle"
+              size={24}
+              color="#000000ff"
+            />
+          ),
+        }}
+      />
 
-                    tabBarIcon: () => ( 
-            <MaterialCommunityIcons name="propane-tank" size={24} color="black" />)}}/>
-            
-              <Tab.Screen name="Carrito" component={CarritoScreen} options={{
-                        tabBarIcon: () => (
-                      <MaterialCommunityIcons name="shopping-search" size={24} color="black" />)}}/>
-            <Tab.Screen name="Mi Cuenta" component={CuentaScreen} options={{
-                  
-                         tabBarIcon: () => (
-                      <MaterialCommunityIcons name="account-circle" size={24} color='#000000ff' />)}}/>
-          
 
-        </Tab.Navigator>
-    )
+
+    </Tab.Navigator>
+  );
 }
 
-function MyStack(){
-    return(
-        <Stack.Navigator screenOptions={{headerShown:false}}>
-            <Stack.Screen name="Welcome" component={WelcomeScreen}/>
-            <Stack.Screen name="Login" component={LoginScreen}/>
-            <Stack.Screen name="Registrarse" component={RegScreen}/>
-            <Stack.Screen name="Home" component={MyTabs}/>
-        </Stack.Navigator>
-    )
+function MyStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Registrarse" component={RegScreen} />
+      <Stack.Screen name="Home" component={MyTabs} />
+    </Stack.Navigator>
+  );
 }
 export default function MainNavigation() {
-    return (
-        <NavigationContainer>
-            <MyStack/>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
 }
