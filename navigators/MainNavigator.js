b73b3099712151c5f0972bce6ac4cf3628704e0d
QuinteroVA <angelqvargas@yahoo,com>
2023-08-05 03:20:25 -0500

    Actualización MainNavigator

--- navigators/MainNavigator.js
+++ navigators/MainNavigator.js
@@ -11,15 +11,7 @@ import CarritoScreen from "../screens/CarritoScreen";
 import HomeScreen from "../screens/HomeScreen"
 //iconos
 import { Feather } from '@expo/vector-icons';
-<<<<<<< HEAD
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-=======
-
-//iconos
-
-import { Feather } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
->>>>>>> e476e557291a44968f677e9ac1abfe14a0dc6d0c
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 function MyTabs() {
